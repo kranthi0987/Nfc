373891a2bbe29b72e4185028c884ae86edfba1e2
sanjay <kranthi0987@gmail.com>
2018-12-06 23:28:58 +0530

    second commit

--- app/src/main/java/com/sanjay/nfc/MainActivity.java
+++ app/src/main/java/com/sanjay/nfc/MainActivity.java
@@ -198,6 +198,13 @@ public static TextView text;
 
             nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
         }
+
+        // Sticky notes received from Android
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            NdefMessage[] messages = getNdefMessages(getIntent());
+            displayMsgs(messages);
+            setIntent(new Intent()); // Consume this intent.
+        }
     }
 
     private void showWirelessSettings() {
@@ -259,8 +266,37 @@ public static TextView text;
     }
 
     private void speakOut(String text) {
-
-
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    //    get ndef
+    NdefMessage[] getNdefMessages(Intent intent) {
+        // Parse the intent
+        NdefMessage[] msgs = null;
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[]{};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[]{
+                        record
+                });
+                msgs = new NdefMessage[]{
+                        msg
+                };
+            }
+        } else {
+            Log.d("ndef", "Unknown intent.");
+            finish();
+        }
+        return msgs;
     }
 }
